b3611bc69e1179daaf5d17805717ca88b6fbc942
huy <huy>
2022-02-14 07:57:50 +0700

    create setup script

---
+++ run.sh
@@ -0,0 +1,4 @@
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/.tmux.config $HOME/.tmux.config
+ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
+ln -s $HOME/dotfiles/alacritty $HOME/.config/alacritty
