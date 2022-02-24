3143597a2fc283b95e7cebdd14888eb8225fe341
Huy Ly <huyly@Huys-MacBook-Pro.local>
2022-02-24 22:35:56 +0700

    update

--- nvim/lua/flutter.lua
+++ nvim/lua/flutter.lua
@@ -67,11 +67,11 @@ require("flutter-tools").setup {
     statusline = {
       -- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
       -- this will show the current version of the flutter app from the pubspec.yaml file
-      app_version = false,
+      app_version = true,
       -- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
       -- this will show the currently running device if an application was started with a specific
       -- device
-      device = false,
+      device = true,
     }
   },
   debugger = { -- integrate with nvim dap + install dart code debugger
