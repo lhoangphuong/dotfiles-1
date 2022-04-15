7b6c8219523c0def47cc3bbc595b387768c78a8d
huy <huy>
2022-04-15 21:54:54 +0700

    update

--- nvim/lua/cmp_setup.lua
+++ nvim/lua/cmp_setup.lua
@@ -44,7 +44,7 @@ cmp.setup({
     ['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
     ['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-u>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({
