e7220f28b724931f48bdbf5f989dd26110c726e8
hlg <TVG@Elcas-Mac-mini.local>
2022-04-20 10:08:13 +0700

    upadte

--- start-working.sh
+++ start-working.sh
@@ -1,7 +1,11 @@
-tmux new-session -t admintool-flutter-run
+if [[$(hostname) != "mac26973.elcaNet.local"]]; then
+	ssh hlg@mac26973.elcaNet.local
+fi
 
-tmux new-window -c ~/admintool/
+tmux new-session -t tng-workspace
 
-tmux new-window -c -/dotfiles/
+tmux new-window -t tng-workspace -c ~/admintool/
+
+tmux new-window -t tng-workspace -c -/dotfiles/
 
 sh ./non-engineer-script/open-app.sh
