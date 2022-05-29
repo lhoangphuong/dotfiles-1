eb03a4ede66c145d51faaa7ea74b5a9f157a3192
Huy Ly <huyly@Huys-MacBook-Pro.local>
2022-05-29 14:49:21 +0700

    update

---
+++ nvim/lua/gruvbox_setup.lua
@@ -0,0 +1,14 @@
+vim.cmd([[
+	colorscheme gruvbox
+
+	hi Comment gui=italic
+	hi link GitSignsCurrentLineBlame Comment
+
+	hi! CursorLine gui=underline cterm=underline guibg=none
+	hi CursorLineNr guibg=NONE
+	hi CursorColumn guibg=#3c3836
+	hi SignColumn guibg=NONE ctermbg=NONE
+
+	hi link NormalFloat  Normal
+	hi link FidgetTask Normal
+]])
