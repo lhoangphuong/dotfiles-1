3c0bac1ac747ea11eda74566be193073bbebc3ea
hlg <TVG@Elcas-Mac-mini.local>
2022-06-16 11:10:13 +0700

    update

---
+++ nvim/lua/default_scheme.lua
@@ -0,0 +1,7 @@
+vim.cmd [[
+  colorscheme default
+  hi clear SignColumn
+  hi DiffAdd ctermbg=none ctermfg=green
+  hi DiffDelete ctermbg=none ctermfg=red
+  hi DiffChange ctermbg=none ctermfg=yellow
+]]
