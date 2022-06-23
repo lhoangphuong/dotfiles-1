44a93f7bc0f09dfc15d9a6b894b65d06f983a893
huy <huy>
2022-06-23 19:12:48 +0700

    update

--- nvim/plugin/sensible.lua
+++ nvim/plugin/sensible.lua
@@ -13,7 +13,7 @@ vim.o.cursorline = true
 vim.wo.colorcolumn='80'
 vim.o.ignorecase = true
 vim.o.smartcase = true
-vim.cmd 'no swapfile'
+vim.cmd 'set noswapfile'
 
 
 local opts = { noremap=true, silent=true }
