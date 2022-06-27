9d7ea4978a234a61ece357eaf9e90d1d49020a11
huy <huy>
2022-06-27 21:32:19 +0700

    change theme

--- nvim/lua/nightly_setup.lua
+++ nvim/lua/nightly_setup.lua
@@ -1,13 +1,16 @@
 -- Lua initialization file
-vim.g.nightflyTransparent = 1
+vim.g.nightflyTransparent = true
 vim.cmd [[
 	colorscheme nightfly
-	hi! CursorLine gui=underline cterm=underline guibg=none
 	hi CursorLineNr guibg=NONE
 	hi WinSeparator guibg=none guifg=#c0caf5
 	hi FLoatBorder guibg=none guifg=#c0caf5
 	hi clear Visual
+	hi clear WinBar
+	hi clear WinBarNC
 	hi PmenuSel guifg=NONE
 	hi link Visual PmenuSel
 	hi link GitSignsCurrentLineBlame Comment
+	hi Comment guifg=#949494
+	hi! CursorLine gui=underline cterm=underline guibg=none
 ]]
